82a93b3d3f7b942b83b672ec4928a684d2defa8a
YoungYunCho <choygyn@gmail.com>
2019-08-05 01:12:18 +0900

    update

---
+++ python_code.py
@@ -0,0 +1,23 @@
+
+print("This notebook is for the capstone project")
+
+The authenticity of host 'github.com (52.78.231.108)' can't be established.
+
+The authenticity of host 'github.com (52.78.231.108)' can't be established.
+
+The authenticity of host 'github.com (52.78.231.108)' can't be established.
+
+The authenticity of host 'github.com (52.78.231.108)' can't be established.
+
+
+The authenticity of host 'github.com (52.78.231.108)' can't be established.
+
+print
+Tprint
+printe 
+
+
+
+
+
+
